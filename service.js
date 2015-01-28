f7413c72fe6ffa954192f09c9ee0983207f29238
Doug Sebastian <dasebast@gmail.com>
2015-01-28 13:46:48 -0700

    done

--- service.js
+++ service.js
@@ -4,9 +4,19 @@ app.service('mainService', function($http, $q) {
 
 
   this.getUsers = function() {
-    return $http({
+    var deferred = $q.defer();
+    $http({
         method: 'GET',
         url: 'http://reqr.es/api/users?page=1'
-    })
-  }
+    }).then(function(response) {
+    	var result = response.data.data;
+    	for (var i = 0; i < result.length; i++) {
+    		result[i].first_name = 'Ralf';
+    	}
+    	deferred.resolve(result);
+    }, function(err) {
+    	deferred.reject(err.statusCode);
+    });
+    return deferred.promise;
+  };
 });
